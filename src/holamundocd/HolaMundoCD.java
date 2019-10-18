6848a75a900131c071e85a2dbbeaf06cadecd78a
agonzalezacevedo <agonzalezacevedo@danielcastelao.org>
2019-10-18 03:36:57 +0200

    Primer Commit

---
+++ src/holamundocd/HolaMundoCD.java
@@ -0,0 +1,11 @@
+package holamundocd;
+/**
+ * @author funny
+ */
+public class HolaMundoCD {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundooo");
+    }
+    
+}
